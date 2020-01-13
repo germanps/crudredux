86e6ed5e7bb15a6581bfa3aefb2bee813b727475
germanps <gpla@pasiona.com>
2020-01-13 11:30:28 +0100

    fromulario para nuevo y listado productos ok

--- src/components/nuevoProducto/NuevoProducto.js
+++ src/components/nuevoProducto/NuevoProducto.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import './NuevoProducto.scss';
 
 const NuevoProducto = () => {
     return(
         <div className="nuevo-producto">
-            <p>NuevoProducto</p>
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="subtitulo text-center mb-4 font-weight-bold ">Agregar Nuevo</h2>
+                            <form>
+                                <div className="form-group">
+                                    <label>Nombre</label>
+                                    <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="Nombre" 
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Precio</label>
+                                    <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="Precio" 
+                                    />
+                                </div>
+
+                                <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Agregar</button>
+                            </form>
+            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/productos/Productos.js
+++ src/components/productos/Productos.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Productos.scss';
 
 const Productos = () => {
     return(
         <div className="productos">
-            <p>Productos</p>
+            <h2 className="subtitulo text-center my-5">Listado de Productos</h2>
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acciones</th>
+                    </tr>   
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
         </div>
     );
 }
