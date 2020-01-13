e3c7e98c69e0632a25308a317cbe48756d64cba3
germanps <gpla@pasiona.com>
2020-01-13 09:17:07 +0100

    redux config && scss base file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Crud redux</h1>
-    </div>
+    <Provider store={store}>
+
+    
+      <div className="App">
+        <h1>Crud redux</h1>
+      </div>
+
+
+    </Provider>
   );
 } 
 
